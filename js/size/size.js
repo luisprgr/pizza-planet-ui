47fd53d5403091357ce35b07721eb62822378d60
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-08-30 15:25:28 -0500

    deleting part of the size endpoint call

--- js/size/size.js
+++ js/size/size.js
@@ -1,5 +1,5 @@
 
-fetch('http://127.0.0.1:5000/size/')
+fetch('http://127.0.0.1:5000/size')
     .then(response => response.json())
     .then(sizes => {
         let rows = sizes.map(element => createSizeTemplate(element));
