70f7c353a86c9364dadf91fb89f9031b23675d80
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 14:18:25 -0500

    [PIZ37] UI bugs and refactor header (#2)
    * feat: header component
    
    * refactor: ui pages
    
    * fix: order detail ingredients price

---
+++ components/header.js
@@ -0,0 +1,36 @@
+class PizzaPlanetHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="header-section container-fluid">
+            <nav class="navbar navbar-expand navbar-dark bg-dark">
+                <a class="navbar-brand" href="/">
+                <img src="/images/image4.png" width="50" height="50" class="d-inline-block align-top" alt="" />
+                L'ultima Cena
+                </a>
+                <ul class="navbar-nav mr-auto"></ul>
+                <ul class="navbar-nav">
+                <!-- LINKS -->
+
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Build Your Pizza!</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/app/order/orders.html">Orders</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/app/ingredient/ingredients.html">Ingredients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/app/size/sizes.html">Sizes</a>
+                </li>
+            </nav>
+            </header>
+        `;
+    }
+}
+
+customElements.define('pizza-planet-header', PizzaPlanetHeader);
